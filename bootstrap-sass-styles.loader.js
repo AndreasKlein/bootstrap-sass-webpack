f8f5b47af4bbc4f29ccfdc4bcbc17f05895234cf
Andreas Klein <andreas@indabamusic.com>
2016-04-07 12:51:07 +0200

    fixes for sass usage

--- bootstrap-sass-styles.loader.js
+++ bootstrap-sass-styles.loader.js
@@ -48,12 +48,12 @@ module.exports = function (content) {
   var config = this.exec(content, this.resourcePath);
   var start =
       "@import          \"~bootstrap-sass/assets/stylesheets/bootstrap/variables\";\n"
-    + "@icon-font-path: \"~bootstrap-sass/assets/fonts/bootstrap/\";\n"
+    + "$icon-font-path: \"~bootstrap-sass/assets/fonts/bootstrap/\";\n"
     + "@import          \"./bootstrap-sass.config.scss\";\n";
   source = start + partials.filter(function (partial) {
     return config.styles[partial];
   }).map(function (partial) {
-    return "@import \"~bootstrap-sass/assets/stylesheets/bootstrap/" + partial;
+    return "@import \"~bootstrap-sass/assets/stylesheets/bootstrap/" + partial + "\";";
   }).join("\n");
   return source;
 }
